fa2cbeea42b356a1ecb634debf4bdc33590bb94e
Renan Ponick <ponickrenan@gmail.com>
2023-04-04 10:43:02 -0300

    fix other way

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,8 @@
-module.exports = { testEnvironment: "node", transform: { '^.+\\.js$': 'babel-jest', }, };
+module.exports = { 
+    testEnvironment: "node",
+    preset: "jest-expo",
+    transform: { '^.+\\.jsx$': 'babel-jest', },
+    transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+    ]
+};
